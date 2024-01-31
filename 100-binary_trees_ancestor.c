52f3fb0f0d5f750b669800ebc18670729f331716
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-01 01:29:59 +0200

    Binary tree ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of 2 nodes
+ * @first: The first node
+ * @second: The second node
+ * 
+ * Return: Lowest common ancestor, or null if not found
+*/
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+    if (first == NULL || second == NULL)
+        return (NULL);
+    
+    binary_tree_t one = *first;
+    binary_tree_t two = *second;
+    binary_tree_t *currentOne = &one;
+    binary_tree_t *currentTwo = &two;
+
+    while(currentOne->parent != NULL)
+    {
+        while(currentTwo->parent != NULL)
+        {
+            if (currentOne->parent == currentTwo->parent)
+                return (currentOne->parent);
+            currentTwo = currentTwo->parent;
+        }
+        currentOne = currentOne->parent;
+    }
+    return (NULL);
+}
