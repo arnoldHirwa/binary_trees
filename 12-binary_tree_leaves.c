d84f3db8476250f12084e11e68898f20dff024a9
Hirwa Arnold <arnold.hirw@gmail.com>
2024-01-31 16:13:18 +0200

    count leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+/**
+* binary_tree_leaves - A function which computes the size of a tree
+* @tree: Head of the binary tree
+*
+* Return: None
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t right = 0, left = 0, mine = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		if (tree->left != NULL)
+		{
+			left = binary_tree_leaves(tree->left);
+		}
+		if (tree->right != NULL)
+		{
+			right = binary_tree_leaves(tree->right);
+		}
+	} else
+	{
+		mine = 1;
+	}
+	return (left + right + mine);
+}
