05c7fee946e6edcac41b755c0de2cbbd0ff1cb5b
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-01 02:13:34 +0200

    new style

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,34 +5,25 @@
 * @first: The first node
 * @second: The second node
 *
-* Return: Lowest common ancestor, or null if not found
+* Return: Lowest common ancestor, or NULL if not found
 */
-
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-				const binary_tree_t *second)
+									const binary_tree_t *second)
 {
-	binary_tree_t one;
-	binary_tree_t two;
-	binary_tree_t *currentOne;
-	binary_tree_t *currentTwo;
+	const binary_tree_t *currentOne;
+	const binary_tree_t *currentTwo;
 
 	if (first == NULL || second == NULL)
 		return (NULL);
 
-	one = *first;
-	two = *second;
-	currentOne = &one;
-	currentTwo = &two;
+	currentOne = first;
+	currentTwo = second;
 
-	while (currentOne->parent != NULL)
+	while (currentOne != currentTwo)
 	{
-		while (currentTwo->parent != NULL)
-		{
-			if (currentOne->parent == currentTwo->parent)
-				return (currentOne->parent);
-			currentTwo = currentTwo->parent;
-		}
-		currentOne = currentOne->parent;
+		currentOne = (currentOne == NULL) ? second : currentOne->parent;
+		currentTwo = (currentTwo == NULL) ? first : currentTwo->parent;
 	}
-	return (NULL);
+
+	return ((binary_tree_t *)currentOne);
 }
