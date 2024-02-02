c8f82671477633a1bb0e4e9909ea973cf7c29060
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-02 16:10:56 +0200

    new way

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -2,53 +2,21 @@
 
 /**
 * binary_traverse - A function which traverse a binary tree in inorder
-* @failed: check if the condition failed
 * @tree: The head ot the tree
+* @min: The minimum value a node can have
+* @max: The maximum value a node can have
 *
 * Return: 1 success, 0 otherwise
 */
 
-int binary_traverse(binary_tree_t *tree, bool *failed)
+int binary_traverse(binary_tree_t *tree, int min, int max)
 {
-	int left, right;
-
-	binary_tree_t *parent;
-
-	if (*failed == true)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (tree->n);
-	if (tree->left != NULL)
-	{
-		left = binary_traverse(tree->left, failed);
-		if (tree->n <= left)
-		{
-			*failed = true;
-			return (0);
-		}
-	}
-	if (tree->right != NULL)
-	{
-		right = binary_traverse(tree->right, failed);
-		if (tree->n >= right)
-		{
-			*failed = true;
-			return (0);
-		}
-	}
-
-	parent = tree->parent;
-	if (parent == NULL)
+	if (tree == NULL)
 		return (1);
-	if (parent->right == tree)
-	{
-		if (tree->left != NULL)
-			return (tree->n < left ? tree->n : left);
-		return (tree->n);
-	}
-	if (tree->right != NULL)
-		return (tree->n > right ? tree->n : right);
-	return (tree->n);
+	if (tree->n < min || tree->n > max)
+		return (0);
+	return (binary_traverse(tree->left, min, tree->n - 1)
+		&& binary_traverse(tree->right, tree->n + 1, max));
 }
 
 /**
@@ -60,14 +28,5 @@ int binary_traverse(binary_tree_t *tree, bool *failed)
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	bool failed = false;
-
-	if (tree == NULL)
-		return (0);
-	binary_traverse((binary_tree_t *)tree, &failed);
-
-	if (failed == true)
-		return (0);
-
-	return (1);
+	return (binary_traverse((binary_tree_t *)tree, -2147483648, 2147483647));
 }
