f1b30ec151b8853ed3969dabfed2b41c804638f0
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-03 05:30:08 +0200

    Fix errors

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - builds an AVL tree from an array
+ * array_to_avl_support - builds an AVL tree from an array
  * @arr: pointer to the first element of the array to be converted
  * @l: left index
  * @r: right index
  * @parent: pointer to the parent node
  * Return: pointer to the root node of the created AVL tree, or NULL on failure
  */
-avl_t *array_to_avl(int *arr, int l, int r, avl_t *parent)
+avl_t *array_to_avl_support(int *arr, int l, int r, avl_t *parent)
 {
 	int index;
 	avl_t *new_node = NULL;
@@ -20,8 +20,8 @@ avl_t *array_to_avl(int *arr, int l, int r, avl_t *parent)
 
 	if (!new_node)
 		return (NULL);
-	new_node->left = array_to_avl(arr, l, index - 1, new_node);
-	new_node->right = array_to_avl(arr, index + 1, r, new_node);
+	new_node->left = array_to_avl_support(arr, l, index - 1, new_node);
+	new_node->right = array_to_avl_support(arr, index + 1, r, new_node);
 	return (new_node);
 }
 
@@ -37,6 +37,6 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 
 	if (!array || size == 0)
 		return (NULL);
-	root = array_to_avl(array, 0, size - 1, root);
+	root = array_to_avl_support(array, 0, size - 1, root);
 	return (root);
 }
