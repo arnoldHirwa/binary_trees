db3f938c685323caa811a070adf7e53d1707f02f
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-03 04:27:08 +0200

    create avl tree

---
+++ 121-avl_insert.c
@@ -0,0 +1,87 @@
+#include "binary_trees.h"
+
+
+
+/**
+ * balance_tree - balance an AVL tree
+ * @tree: pointer to the root node of the tree to balance_tree
+ * @node: pointer to the node to be inserted
+ * Return: pointer to the new root node of the tree once balance_treed
+ */
+avl_t *balance_tree(avl_t *tree, avl_t *node)
+{
+	int balance_factor;
+
+	if (!tree)
+		return (NULL);
+
+	balance_factor = binary_tree_balance(tree);
+
+	if (balance_factor > 1)
+	{
+		/* left-right case */
+		if (tree->left->n < node->n)
+			tree->left = binary_tree_rotate_left(tree->left);
+		return (binary_tree_rotate_right(tree));
+	}
+	if (balance_factor < -1)
+	{
+		/* right-left case */
+		if (tree->right->n > node->n)
+			tree->right = binary_tree_rotate_right(tree->right);
+		return (binary_tree_rotate_left(tree));
+	}
+	return (tree);
+}
+
+/**
+ * insert - Inserts a value in an AVL Tree
+ * @tree: Pointer to the root node of the AVL tree for inserting the value
+ * @node: Node to be inserted
+ * Return: Pointer to the created node, or NULL on failure
+ */
+avl_t *insert(avl_t *tree, avl_t *node)
+{
+	if (!node)
+		return (NULL);
+	if (!tree)
+		return (node);
+	if (tree->n > node->n)
+	{
+		tree->left = insert(tree->left, node);
+		tree->left->parent = tree;
+	}
+	else if (tree->n < node->n)
+	{
+		tree->right = insert(tree->right, node);
+		tree->right->parent = tree;
+	}
+	else if (tree->n == node->n)
+	{
+		free(node);
+		node = NULL;
+		return (tree);
+	}
+
+	return (balance_tree(tree, node));
+}
+
+/**
+ * avl_insert - Inserts a value in an AVL Tree
+ *
+ * @tree: Pointer to the root node of the AVL tree for inserting the value
+ * @value: Value to store in the node to be inserted
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	avl_t *new_node = NULL;
+
+	if (!tree)
+		return (NULL);
+
+	new_node = binary_tree_node(NULL, value);
+	*tree = insert(*tree, new_node);
+	return (new_node);
+}
