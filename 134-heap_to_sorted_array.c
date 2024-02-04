6f027bf327902f664e0a70a3a49dabf1fec6260c
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-04 02:17:38 +0200

    heap sort again

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -28,7 +28,7 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 	size_t i;
 
 	*size = nbr_nodes(heap);
-	array = malloc(*size);
+	array = malloc(*size * sizeof(int));
 	if (array == NULL)
 		return (NULL);
 
